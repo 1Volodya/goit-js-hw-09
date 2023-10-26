78789fad5bba7f96d987d95fc4794007b9f788e7
Volodymyr Koval <vovankoval1996@gmail.com>
2023-10-27 01:37:56 +0300

    done 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    const obj = {
+      position,
+      delay,
+    };
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(obj);
+      } else {
+        reject(obj);
+      }
+    }, delay);
+  });
+  return promise;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let currentDelay = parseInt(form.elements.delay.value);
+  const stepValue = parseInt(form.elements.step.value);
+  const amountValue = parseInt(form.elements.amount.value);
+
+  for (let i = 1; i <= amountValue; i++) {
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    currentDelay += stepValue;
   }
 }
+
+form.addEventListener('submit', handleSubmit);
